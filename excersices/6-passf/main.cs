ef2ab5a07ab2aa49ce4bb591ea7299dc0ead155a
Lasse Schwaner Arndt <lasse@s-arndt.dk>
2022-02-23 10:21:24 +0100

    did excercise 7

--- excersices/6-passf/main.cs
+++ excersices/6-passf/main.cs
@@ -10,8 +10,8 @@ class main{
 		
 	//Method for constructing table with given a,b,dx
 	static void make_table(System.Func<double,double> f, double a, double b, double dx){
-		WriteLine($"for a={a}, b={b}, dx={dx} ");
-		for double( x=a;x<=b;x+=dx)WriteLine($"{x}, {f(x)}");
+		WriteLine($"for a={a}, b={b}, dx={dx}");
+		for(double x=a;x<=b;x+=dx)WriteLine($"{x}, {f(x)}");
 		}
 	
 	
@@ -24,10 +24,15 @@ class main{
 	System.Func<double,double> sin = delegate(double x){return Sin(x);};
 	System.Func<double,double> sin2 = delegate(double x){return Sin(2*x);};
 	System.Func<double,double> sin3 = delegate(double x){return Sin(3*x);};
+	
+	WriteLine("\nsin(x) function");
+	make_table(sin, 0, 10, 1);
 
-	make_table10(square);
-	make_table10(sin);
+	WriteLine("\nsin(2x) function");
+	make_table(sin2, 0, 10, 1);
+
+	WriteLine("\nsin(3x) function");
+	make_table(sin3, 0, 10, 1);
 	}
-
-
 }
+
