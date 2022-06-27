6979020cbd6ce060f02f1a7a54a03d85df39ed0e
LaArndt <lasse@s-arndt.dk>
2022-06-27 14:46:31 +0200

    finished roots A

--- Homeworks/roots/A/main.cs
+++ Homeworks/roots/A/main.cs
@@ -56,11 +56,7 @@ public static vector NewtonRoots(Func<vector,vector>f // input function vectors 
 		
 
 public static void Main(){
-	/*Func<vector, vector> rosenbrock = delegate(vector vec){
-	double x = (double)vec[0];
-	double y = (double)vec[1];
-	return (1-x)*(1-x)+100*(y-x*x)*(y-x*x);
-	};*/
+
 
 	// 1D function test //
 	Func<vector,vector> f1d = delegate(vector vec){
@@ -69,14 +65,11 @@ public static void Main(){
 		f[0] = x*x-3*x;
 		return f;
 	};
-	Error.WriteLine("test1");;
 	vector x0 =  new double[]{0.1};
 	vector x02 =  new double[]{2.9};
-	Error.WriteLine("test2");
 	vector res1d = NewtonRoots(f1d,x0, 1e-16);
 	vector res1d2 = NewtonRoots(f1d,x02, 1e-16);
-	Error.WriteLine("test3");
-	Error.WriteLine($"roots of 1d function x^2-3x are {res1d[0]} and {res1d2[0]} (0,3) expected" );
+	Error.WriteLine($"roots of 1d function x^2-3x are ({res1d[0]} and {res1d2[0]}): (0,3) expected" );
 
 
 	// 2D function test //
@@ -95,20 +88,22 @@ public static void Main(){
 	vector sol2D = NewtonRoots(f2D,root2D,1e-16);
 	Error.WriteLine($"roots of function x^2+2y+8xy are {sol2D[0]} & {sol2D[1]} \n inserted into the expression this returns (-0.25)^2+2*0.0625+8*(-0.25)*0.0625=0.0625");
 		
-	Func<vector,vector> f2d =  delegate(vector vec){
-		vector f = new vector(2);
-		double x = vec[0];
-		double y = vec[1];
-	
-		//f = x*x*x +3*y*y + x^2*y^4
-		f[0] = 3*x*x+4*x*x*y*y*y;
-		f[1] = 6*y+2*x*y*y*y*y;
+	Error.WriteLine("Testing using Rosenbrocks valley function");
+	Func<vector, vector> rosenbrock = delegate(vector vec){
+		double x = (double)vec[0];
+		double y = (double)vec[1];
+		//function is (1-x)^2+100(y-x^2)²)
+		//extremums can be found at dy/dx = 0
+
+		vector f =  new vector(2);
+		f[0] = 2*x-2+100*(4*x*x*x - 4*y*x);
+		f[1] = 100*(1-2*x*x);
 		return f;
 	};
-	vector guess = new double[]{5,5};
-	vector sol2 = NewtonRoots(f2d,guess,1e-16);
-	Error.WriteLine($"x^3 +3y^2 has roots: {sol2[0]} and {sol2[1]}");
-
+	
+	vector rootRose = new double[]{1,1};
+	vector solRose  = NewtonRoots(rosenbrock,rootRose,1e-16);
+	Error.WriteLine($"rosenbrock extremums found at x={solRose[0]} and y={solRose[1]}");
 
 }//Main method
 
